6b6a074130d496d576d383e17d57504bd9ab00b4
rmuell9 <rmatthewmueller@gmail.com>
2025-09-23 12:59:15 -0500

    A

---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class Bender(Enum):
+    AIR_BENDER = "air"
+    WATER_BENDER = "water"
+    EARTH_BENDER = "earth"
+    FIRE_BENDER = "fire"
+
+
+class TextNode(text, text_type, url=None):
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+
+def __eqr__():
+    pass
+
+
+def __repr__():
+    pass
