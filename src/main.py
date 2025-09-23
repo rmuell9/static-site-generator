f745b3885f65cd7ebb4624a9a3ddf7e5b7c01563
rmuell9 <rmatthewmueller@gmail.com>
2025-09-23 13:20:59 -0500

    B

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextType
+from textnode import TextNode
+
+
+def main():
+    dummy = TextNode("hello", TextType.LINK, "google.com")
+    print(dummy)
+
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,21 +1,24 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    TEXT = "Text"
+    BOLD_TEXT = "**Bold text**"
+    ITALIC_TEXT = "_Italic text_"
+    CODE_TEXT = "`Code text`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
 
 
-class TextNode(text, text_type, url=None):
-    self.text = text
-    self.text_type = text_type
-    self.url = url
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
+    def __eq__(self, other):
+        return (self.text == other.text and 
+                self.text_type == other.text_type and 
+                self.url == other.url)
 
-def __eqr__():
-    pass
-
-
-def __repr__():
-    pass
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
