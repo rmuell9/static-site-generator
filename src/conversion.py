170c2643f0bfee9707218b012e5f5720ba155e0f
rmuell9 <rmatthewmueller@gmail.com>
2025-10-07 01:37:23 -0500

    stuff

--- src/conversion.py
+++ src/conversion.py
@@ -1,6 +1,6 @@
 import re
 from textnode import TextType, TextNode
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 from blocks import BlockType
 
 
@@ -26,6 +26,7 @@ def text_node_to_html_node(text_node):
 
     raise ValueError("Invalid TextType")
 
+
 #Create TextNodes from raw markdown strings
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -47,15 +48,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     return new_nodes
 
+
 #Convert raw markdown to alt and href touples for images and links
 def extract_markdown_images(text):
-    images = re.findall(r"!\[(.*?)\]\((https?://.*?)\)", text)
+    images = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     return images
 
+
 def extract_markdown_links(text):
-    images = re.findall(r"\[(.*?)\]\((https?://.*?)\)", text)
+    images = re.findall(r"\[(.*?)\]\((.*?)\)", text)
     return images
 #Split raw markdown text into TextNodes based on images and links
+
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -63,19 +67,18 @@ def split_nodes_image(old_nodes):
         if node.text_type is not TextType.TEXT:
             new_nodes.append(node)
         else:
-            extract = extract_markdown_images(node.text)
-            text_types = list(filter(None, re.split(r"!\[.*?\]\(.*?\)", node.text)))
-            ecount = 0
-            tcount = 0
-            for i in range(len(extract) + len(text_types)):
-                if i % 2 == 0:
-                    new = TextNode(text_types[tcount], TextType.TEXT)
-                    new_nodes.append(new)
-                    tcount += 1
-                else:
-                    new = TextNode(extract[ecount][0], TextType.IMAGE, extract[ecount][1])
-                    new_nodes.append(new)
-                    ecount += 1
+            current_text = node.text
+            images = extract_markdown_images(current_text)
+            
+            for alt_text, url in images:
+                parts = current_text.split(f"![{alt_text}]({url})", 1)
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                current_text = parts[1]
+            
+            if current_text:
+                new_nodes.append(TextNode(current_text, TextType.TEXT))
     return new_nodes
 
 def split_nodes_link(old_nodes):
@@ -84,22 +87,20 @@ def split_nodes_link(old_nodes):
         if node.text_type is not TextType.TEXT:
             new_nodes.append(node)
         else:
-            extract = extract_markdown_links(node.text)
-            text_types = list(filter(None, re.split(r"\[.*?\]\(.*?\)", node.text)))
-            ecount = 0
-            tcount = 0
-            for i in range(len(extract) + len(text_types)):
-                if i % 2 == 0:
-                    new = TextNode(text_types[tcount], TextType.TEXT)
-                    new_nodes.append(new)
-                    tcount += 1
-                else:
-                    new = TextNode(extract[ecount][0], TextType.LINK, extract[ecount][1])
-                    new_nodes.append(new)
-                    ecount += 1
+            current_text = node.text
+            links = extract_markdown_links(current_text)
+            
+            for link_text, url in links:
+                parts = current_text.split(f"[{link_text}]({url})", 1)
+                if parts[0]:
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(link_text, TextType.LINK, url))
+                current_text = parts[1]
+            
+            if current_text:
+                new_nodes.append(TextNode(current_text, TextType.TEXT))
     return new_nodes
 
-#Bringing it all together
 def text_to_textnodes(text):
     input = [TextNode(text, TextType.TEXT)]
     bold = split_nodes_delimiter(input, "**", TextType.BOLD)
@@ -110,17 +111,17 @@ def text_to_textnodes(text):
     return link
 
 
-
 def markdown_to_blocks(markdown):
     res = []
     for n in markdown.split("\n\n"):
-        res.append(n.strip())
+        n = n.strip()
+        if n != '':
+            res.append(n)
     return res
 
 
 def block_to_blocktype(block):
     lines = block.split("\n")
-
     if block[0:3] == "```" and block[-3:] == "```":
         return BlockType.CODE
     elif block[0] == "#" and "#######" not in block:
@@ -140,17 +141,44 @@ def block_to_blocktype(block):
     return BlockType.PARAGRAPH
 
 
-
-
-
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-    print(blocks)
+    children = []
     for block in blocks:
         blocktype = block_to_blocktype(block)
-#
-# test = '''# Hey
-#
-# ```sup```'''
-#
-# markdown_to_html_node(test)
+        if blocktype == BlockType.HEADING:
+            level = block.count("#", 0, 6)
+            text = block.lstrip("# ").strip()
+            textnodes = text_to_textnodes(text)
+            htmlnodes = [text_node_to_html_node(node) for node in textnodes]
+            children.append(ParentNode(f"h{level}", htmlnodes))
+        elif blocktype == BlockType.CODE:
+            text = block.strip("```").strip()
+            children.append(ParentNode("pre", [ParentNode("code", [LeafNode(None, text)])]))
+        elif blocktype == BlockType.QUOTE:
+            text = "\n".join(line.lstrip("> ") for line in block.split("\n"))
+            textnodes = text_to_textnodes(text)
+            htmlnodes = [text_node_to_html_node(node) for node in textnodes]
+            children.append(ParentNode("blockquote", htmlnodes))
+        elif blocktype in [BlockType.UNORDEREDLIST, BlockType.ORDEREDLIST]:
+            tag = "ul" if blocktype == BlockType.UNORDEREDLIST else "ol"
+            list_items = []
+            for line in block.split("\n"):
+                text = line.lstrip("- ").split(". ", 1)[-1]
+                textnodes = text_to_textnodes(text)
+                htmlnodes = [text_node_to_html_node(node) for node in textnodes]
+                list_items.append(ParentNode("li", htmlnodes))
+            children.append(ParentNode(tag, list_items))
+        else:
+            textnodes = text_to_textnodes(block)
+            htmlnodes = [text_node_to_html_node(node) for node in textnodes]
+            children.append(ParentNode("p", htmlnodes))
+    return ParentNode("div", children)
+
+def extract_title(markdown):
+    target = "# "
+    if target in markdown:
+        for line in markdown.split("\n"):
+            if line[:2] == target:
+               return line.lstrip(target)
+    raise Exception("No title")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,60 @@
+import os
+import sys
+import shutil
 import conversion
-from textnode import TextType, TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode
 
 
 def main():
-    pass
+    try:
+        basepath = sys.argv[1]
+    except:
+        basepath = "/"
+    def public(copy):
+        dest = "public/"
+        if os.path.exists(dest) and os.path.exists(copy):
+            for thing in os.listdir(dest):
+                target = dest + thing
+                if os.path.isfile(target):
+                    os.remove(target)
+                elif os.path.isdir(target):
+                    shutil.rmtree(target)
+            if os.listdir(copy) is not None:
+                dir_to_public(copy, dest)
+
+    def dir_to_public(dir, dest):
+        for thing in os.listdir(dir):
+            target = dir + thing
+            if os.path.isfile(target):
+                shutil.copy(target, dest)
+            if os.path.isdir(target):
+                os.mkdir(dest + thing)
+                dir_to_public(target + "/", dest + thing + "/")
+
+    def generate_page(from_path, template_path, dest_path, basepath):
+        print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+        md_content = open(from_path).read()
+        template_content = open(template_path).read()
+        html_string = conversion.markdown_to_html_node(md_content).to_html()
+        title = conversion.extract_title(md_content)
+        res = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
+        open(dest_path, "x").write(res)
+
+    def generate_page_rec(dir_path_content, template_path, dest_dir_path, basepath):
+        if not os.path.exists(dest_dir_path):
+            os.makedirs(dest_dir_path)
+            
+        for thing in os.listdir(dir_path_content):
+            source_path = os.path.join(dir_path_content, thing)
+            if os.path.isfile(source_path) and thing.endswith(".md"):
+                filename = thing[:-3] + ".html"
+                dest_file_path = os.path.join(dest_dir_path, filename)
+                generate_page(source_path, template_path, dest_file_path, basepath)
+            elif os.path.isdir(source_path):
+                dest_sub_dir = os.path.join(dest_dir_path, thing)
+                generate_page_rec(source_path, template_path, dest_sub_dir, basepath)
+
+
+    public("static/")
+    generate_page_rec("content/", "template.html", "public/", basepath)
 main()
